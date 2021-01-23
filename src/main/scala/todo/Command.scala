52b4a863da22c13b174f3a8a49510c94560607bb
njin <njin.cool@gmail.com>
2021-01-23 15:38:37 +0800

    feature: todo list command

--- src/main/scala/todo/Command.scala
+++ src/main/scala/todo/Command.scala
@@ -25,12 +25,31 @@ class DoneCommand(index: Int) extends Command {
   }
 }
 
+case class ListCommand(isShowAll: Boolean = false) extends Command {
+  override def execute(state: State): String = {
+    val builder = new StringBuilder
+    val items = if (isShowAll) state.listAll else state.list
+
+    items.foreach {
+      case Item(id, content, Status.available) => builder.append(s"$id. $content\n")
+      case Item(id, content, Status.done) => builder.append(s"$id. [Done]$content\n")
+    }
+
+    builder.append(s"Total: ${items.length} items")
+    if (isShowAll) builder.append(s", ${state.doneCount} item done")
+
+    builder.toString
+  }
+}
+
 object Command {
   def apply(args: Array[String]): Command = args match {
     case Array("add", content) => new AddCommand(content)
     case Array("done", idStr) =>
       val id = idStr.toInt
       new DoneCommand(id)
+    case Array("list") => ListCommand()
+    case Array("list", "--all") => ListCommand(true)
     case _ => new InvalidCommand
   }
 }
